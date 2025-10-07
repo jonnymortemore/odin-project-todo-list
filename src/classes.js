a15a38c4d7d09c04184ddb5412afea2f466c333c
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-10-07 16:31:08 +1100

    Updated class ids
    Using ever scaling numbers within parent classes
    to track the IDs of their children

--- src/classes.js
+++ src/classes.js
@@ -10,30 +10,49 @@ class SupportFunc {
     }
 }
 
-
 class webApp {
+
+    #page_id = 0
+    #label_id = 0
+
     constructor() {
         this.pages = [
-            new Page("Page 1")
-        ]
+            new Page("Page 1", this.#page_id)
+        ];
+        // will hold all created labels
+        this.labels = [
+            new Label("Important", "#FF0000", this.#label_id),
+        ];
     }
     createPage(pageName) {
-        this.pages.push(new Page(pageName, this.pages.length));
+        this.#page_id++
+        this.pages.push(new Page(pageName, this.#page_id));
     }
-    deletePage(id) {
-        this.pages.splice(id, 1);
+    deletePage(page) {
+        //find pages in array and remove
+    }
+
+    createLabel(labelName, color) {
+        //check color correct format
+        this.#label_id++
+        this.labels.push(new Label(labelName, color, this.#label_id))
+    }
+    deleteLabel(label) {
+        //find label in array and remove
     }
 
 }
 
-
 class Page extends SupportFunc {
-    constructor(pageName, id = 0) {
+
+    #list_id = 0;
+
+    constructor(pageName, id) {
         this.id = id;
         this.name = pageName;
         this.dateCreated = this.getNow()
         this.lists = [
-            new List("List 1")
+            new List("List 1", this.#list_id)
         ]
     }
     //getting and setting page name
@@ -43,53 +62,69 @@ class Page extends SupportFunc {
     get name() {
         return this._name;
     }
+
     createList(listName) {
+        this.#list_id++
         this.lists.push(
             new List(
                 listName, 
-                this.lists.length
+                this.#list_id
             )
         )
 
     }
-    deleteList(id) {
-        this.lists.splice(id, 1);
+    deleteList(list) {
+        //find list and delete from array
     }
 }
  
 class List extends SupportFunc {
-    constructor(listName, id = 0) {
+
+    #task_id = 0;
+
+    constructor(listName, id) {
         this.id = id;
         this.name = listName;
         this.dateCreated = this.getNow();
         this.tasks = [
-            new Task("Task 1", null)
+            new Task("Task 1", null, this.#task_id)
         ]
     }
+
+    set name(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+
     createTask(taskName, finishDate) {
+        this.#task_id++
         this.tasks.push(
             new Task(
                 taskName, 
                 finishDate,  
-                this.tasks.length
+                this.#task_id
             )
         )
     }
-    deleteTask(id) {
-        this.tasks.splice(id, 1);
+    deleteTask(task) {
+        //find task in array and remove
     }
 
 
 }
 
 class Task extends SupportFunc {
-    constructor(taskName, finishDate, id = 0) {
+    constructor(taskName, finishDate, id) {
         this.id = id;
         this.title = taskName;
         this.dateCreated = this.getNow();
         this.completionDate = finishDate;
         this.description = "";
         this.completed = false;
+        // will hold all added labels
+        this.label = []
     }
 
     set title(title) {
@@ -115,5 +150,32 @@ class Task extends SupportFunc {
     get description() {
         return this._description
     }
+
+    addLabel(label) {
+        this.labels.push(label);
+    }
 }
 
+class Label {
+    constructor(labelName, color, id) {
+        this.name = labelName;
+        this.color = color;
+        this.id = id;
+    }
+
+    set color(color) {
+        //check correct format
+        this._color = color
+    }
+
+    get color() {
+        return this._color
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
