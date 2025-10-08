f3dd562e06b6f916262ed10ea09942bf02056b37
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-10-08 12:14:23 +1100

    save current page to webAppDom

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,8 @@ import {WebApp, Page, List, Task, Label} from "./classes"
 export class WebAppDom {
     constructor() {
         this.webApp = new WebApp
-        this.loadPage(this.webApp.pages[0])
+        this.currentPage = this.webApp.pages[0];
+        this.loadPage(this.currentPage)
     }
     loadPage(page) {
         //generate selected page
@@ -11,7 +12,6 @@ export class WebAppDom {
         pageElement.className = "page";
         pageElement.innerText = page.name;
         document.querySelector("main").appendChild(pageElement);
-
     }
     loadList() {
 
